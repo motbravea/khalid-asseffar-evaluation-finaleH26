adf2eff741175ef2c57834283893ce23c00403f9
Khalid Asseffar <khalid@email.com>
2026-06-17 10:20:33 -0400

    Preparation de la connexion Railway

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,21 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 db.connect((err) => {
-    if(err){
-        console.log(err);
-    } else {
-        console.log("MySQL connecté");
-    }
+  if (err) {
+    console.error("Erreur de connexion MySQL :", err);
+    return;
+  }
+
+  console.log("MySQL connecté");
 });
 
 module.exports = db;
